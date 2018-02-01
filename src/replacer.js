31b07f24961c167c0f00f242f5120086c40e0b01
Federico Vitale <fedevitale99@gmail.com>
2018-02-01 18:19:29 +0100

    core

---
+++ src/replacer.js
@@ -0,0 +1,13 @@
+$(function () {
+	const matcher = /([dD]onald\s{1,2}[tT]rump|[dD]onald\s{1,2}[jJ](\.|)\s{1,2}[tT]rump)/g;
+	let dts_debug = false;
+
+	if (dts_debug)
+		console.log('Donald Trump Shit READY');
+
+	$('*').each(function () {
+		var $this = $(this);
+		if (!$this.children().length)
+			$this.text($this.text().replace(matcher, '💩'));
+	});
+})
